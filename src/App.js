90fc90b952f4b6c453463538abf3f2a98308d39f
Neil Froggatt <neilfr@live.com>
2023-02-15 12:15:49 -0500

    trying again

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ const App = () => {
     return(
         <div>
             <h1>
-                Hello World
+                Basic
             </h1>
             <BrowserRouter>
                 <Routes>
